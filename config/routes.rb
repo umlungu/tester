6f69c7774b116067f404ce4ed7bd652312a7556d
Felix Norton <felix@succinctdesign.co.za>
2014-02-25 13:08:54 +0200

    contact kinda working

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Tester::Application.routes.draw do
 
 
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
